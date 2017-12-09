2c796284e557a91bd4aa8870afd7ae14395da493
Faiz Umar Baraja <faiz@Faizs-MacBook-Pro.local>
2017-12-09 15:56:03 +0700

    Commit : 1. Setup project

---
+++ RuangGuru Trivia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RuangGuru Trivia
+//
+//  Created by Faiz Umar Baraja on 09/12/2017.
+//  Copyright © 2017 FaizBarajaApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
